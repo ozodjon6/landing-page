b4c49e9dcee649a64edb9552faf25ebc4506989e
Ozodbek <o.jumaev@safenetpay.com>
2023-02-16 00:21:36 +0500

    add header and footer part

--- app/js/common.js
+++ app/js/common.js
@@ -1,31 +1,11 @@
-// const openBtn = document.querySelector(".header-page__btn:last-child");
-// const openSignBtn = document.querySelector(".header-page__btn:first-child");
-// const popupPage = document.querySelector(".popup-page");
-// const signPage = document.querySelector(".sign-page");
-// const overlay = document.querySelector(".overlay");
-// const closeBtn = document.querySelectorAll(".close-btn");
+window.addEventListener('DOMContentLoaded', () => {
+    const menuList = document.querySelector(".menu-list");
+    const hamburger = document.querySelector(".hamburger");
 
 
-// openBtn.addEventListener("click", function() {
-//     popupPage.classList.add("active")
-//     overlay.classList.add("active")
-// })
+    hamburger.addEventListener("click", function(e) {
+        menuList.classList.toggle("open-modal");
+        hamburger.classList.toggle("open-modal");
+    });
 
-// openSignBtn.addEventListener("click", function() {
-//     signPage.classList.add("active")
-//     overlay.classList.add("active")
-// })
-
-// for (let i = 0; i < closeBtn.length; i++) {
-//     closeBtn[i].addEventListener("click", function() {
-//         popupPage.classList.remove("active");
-//         signPage.classList.remove("active");
-//         overlay.classList.remove("active")
-//     })
-// }
-
-// overlay.addEventListener("click", function() {
-//     popupPage.classList.remove("active");
-//     signPage.classList.remove("active");
-//     this.classList.remove("active")
-// })
+});
