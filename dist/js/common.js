e52b56b2598671874fed86cc49478dd735578a69
Ozodbek <o.jumaev@safenetpay.com>
2023-02-17 01:31:15 +0500

    done first page continue second page

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,11 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menuList = document.querySelector(".menu-list");
     const hamburger = document.querySelector(".hamburger");
+    const selectedProjectsItem = document.querySelectorAll('.selected-projects__item');
 
 
     hamburger.addEventListener("click", function(e) {
         menuList.classList.toggle("open-modal");
         hamburger.classList.toggle("open-modal");
+    });
+
+    selectedProjectsItem.addEventListener("click", function(e) {
+        e.preventDefault();
     });
 
 });
